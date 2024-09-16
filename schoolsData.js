89469b3bf60580a6613bec1dc4a428fbbeb45fa4
Rax <91789830+rax0609@users.noreply.github.com>
2024-09-16 21:59:25 +0800

    部分學校錯誤連結修改

---
+++ schoolsData.js
@@ -0,0 +1,88 @@
+// schoolsData.js
+const schoolsData = [
+    {
+        name: '臺灣大學',
+        websites: [
+            { url: 'https://www.ntu.edu.tw', description: '臺灣大學首頁' },
+            { url: 'https://my.ntu.edu.tw/', description: '臺灣大學 MyNTU' },
+            { url: 'https://www.lib.ntu.edu.tw/', description: '臺灣大學 圖書館' },
+            { url: 'https://if192.aca.ntu.edu.tw/index.php', description: '臺灣大學 網路選課系統' },
+            { url: 'https://moodle.ntu.edu.tw', description: '臺灣大學 Moodle' }
+        ]
+    },
+    {
+        name: '成功大學',
+        websites: [
+            { url: 'https://www.ncku.edu.tw', description: '成功大學首頁' },
+            { url: 'https://eportfolio.ncku.edu.tw', description: '成功大學 EPortfolio' }
+        ]
+    },
+    {
+        name: '清華大學',
+        websites: [
+            { url: 'https://www.nthu.edu.tw', description: '清華大學首頁' },
+            { url: 'https://moodle.nthu.edu.tw', description: '清華大學 Moodle' }
+        ]
+    },
+    {
+        name: '交通大學',
+        websites: [
+            { url: 'https://www.nctu.edu.tw', description: '交通大學首頁' },
+            { url: 'https://elearn.nctu.edu.tw', description: '交通大學 Moodle' }
+        ]
+    },
+    {
+        name: '南臺科技大學',
+        websites: [
+            { url: 'https://www.nutn.edu.tw', description: '南臺科技大學首頁' },
+            { url: 'https://eportal.stust.edu.tw/', description: '南臺科技大學 E網通' },
+            { url: 'https://portal.stust.edu.tw/abs_stu/checkin.asp', description: '南臺科技大學 請假系統' },
+            { url: 'https://course.stust.edu.tw/CourSel/Login.aspx', description: '南臺科技大學 選課系統' },
+            { url: 'https://flipclass.stust.edu.tw/', description: '南臺科技大學 FlipClass' },
+            { url: 'http://lib.stust.edu.tw/', description: '南臺科技大學 圖書館' },
+            { url: 'https://portal.stust.edu.tw/studclub/Login.aspx', description: '南臺科技大學 社團課程管理系統' },
+            { url: 'https://portal.stust.edu.tw/CourseWaived/Login.aspx', description: '南臺科技大學 學分抵免系統' },
+            { url: 'https://webap.stust.edu.tw/pwd/Index.aspx', description: '南臺科技大學 校務帳號密碼啟用/變更入口系統' }
+        ]
+    },
+    {
+        name: '國立中山大學',
+        websites: [
+            { url: 'https://www.nsysu.edu.tw', description: '中山大學首頁' },
+            { url: 'https://elearn.nsysu.edu.tw', description: '中山大學 Moodle' },
+            { url: 'https://www.lib.nsysu.edu.tw', description: '中山大學 圖書館' }
+        ]
+    },
+    {
+        name: '國立政治大學',
+        websites: [
+            { url: 'https://www.nccu.edu.tw', description: '政治大學首頁' },
+            { url: 'https://elearning.nccu.edu.tw', description: '政治大學 e-Learning' },
+            { url: 'https://www.lib.nccu.edu.tw', description: '政治大學 圖書館' }
+        ]
+    },
+    {
+        name: '國立臺灣師範大學',
+        websites: [
+            { url: 'https://www.ntnu.edu.tw', description: '臺灣師範大學首頁' },
+            { url: 'https://toc.co.ntnu.edu.tw', description: '臺灣師範大學 TOC' },
+            { url: 'https://moodle.ntnu.edu.tw', description: '臺灣師範大學 Moodle' }
+        ]
+    },
+    {
+        name: '國立臺灣科技大學',
+        websites: [
+            { url: 'https://www.ntust.edu.tw', description: '臺灣科技大學首頁' },
+            { url: 'https://i.ntust.edu.tw/student', description: '臺灣科技大學 校務行政系統(學生)' },
+            { url: 'https://library.ntust.edu.tw/', description: '臺灣科技大學 圖書館' }
+        ]
+    },
+    {
+        name: '臺北醫學大學',
+        websites: [
+            { url: 'https://www.tmu.edu.tw', description: '臺北醫學大學首頁' },
+            { url: 'https://newacademic.tmu.edu.tw/', description: '臺北醫學大學 教務學務系統' },
+            { url: 'https://library.tmu.edu.tw/', description: '臺北醫學大學 圖書館' }
+        ]
+    }
+];
